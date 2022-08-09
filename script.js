5920d1aca905e702537f046132c8ffadbe012df0
Guilherme <guilhermeisk@hotmail.com>
2022-08-09 12:17:50 -0300

    Alteração

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*
+  função para checar as informaçãoes de login.
+  Pega o valor digitado pelo usuario e compara com o valor das variaveis, caso algo não esteja de acordo, gera um alerta informando que esta errado, e ira alterar o tipo do display do elemento html, utilizando css, para que tenha a opção de relembrar a senha , caso os valores passados baterem fará login normalmente sendo redirecionado para outra pagina
+*/
 let usuario='rntcursos@gmail.com'
 let password='12345678'
 
@@ -6,10 +10,10 @@ function check_login(){
   let senha=document.getElementById('senha').value
 
   if(usuario== email && password==senha){
-    document.location.href='login.html'
+    document.location.href='login.html'//sendo redirecionado
   } else{
     alert('Usuario ou senha errados')
-    document.getElementById('forget').style.display='flex'
+    document.getElementById('forget').style.display='flex'//desocultando elemento
   }
 
 }
